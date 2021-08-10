d4d90e8c4028628286b821bf3d65e30591d72161
Luke Barkhuizen <Lukebarkhuizen@standardbank.co.za>
2021-08-10 19:32:01 +0200

    Initial Commit

---
+++ RGBToHex/RGBToHexApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBToHexApp.swift
+//  RGBToHex
+//
+//  Created by Luke Barkhuizen on 2021/08/10.
+//
+
+import SwiftUI
+
+@main
+struct RGBToHexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
